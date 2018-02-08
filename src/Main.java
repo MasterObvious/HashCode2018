735db97d9cfccd65ffb71b9759cb39401ebd853d
MasterObvious <me@timlazarus.com>
2018-02-08 22:05:34 +0000

    Added hello int

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String args[]){
         System.out.println("Hello World");
+
+        int hello = 1;
+        hello = hello * 2;
+
+        System.out.println(hello);
     }
 }
