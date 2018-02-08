923c37c323454c6a351d99bf844ea54c010f6fb1
Dan <09harrisda@gmail.com>
2018-02-08 22:34:38 +0000

    stupid change

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String args[]){
-        System.out.println("Hello World");
+        System.out.println("Helo World");
 
         int hello = 1;
         hello = hello * 2;
