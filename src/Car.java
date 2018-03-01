d201432a1629413193b07e6ce1b75afc8b73bfba
Franz <fgn24@cam.ac.uk>
2018-03-01 20:57:45 +0000

    Change heuristics

--- src/Car.java
+++ src/Car.java
@@ -4,17 +4,18 @@ public class Car {
     public int x;
     public int y;
     public int freeFrom;
-    private PriorityQueue<RideWithDistance> sortedRides;
+    private LinkedList<RideWithDistance> sortedRides;
 
     public Car(Collection<Ride> rides){
         x = 0;
         y = 0;
         freeFrom = 0;
         Comparator<RideWithDistance> comparator = new RideComparator();
-        sortedRides = new PriorityQueue<>(comparator);
+        sortedRides = new LinkedList<>();
         for(Ride r : rides) {
             sortedRides.add(new RideWithDistance(r,this));
         }
+        Collections.sort(sortedRides, comparator);
     }
     public void update(Integer timeStep) {
         sortedRides = sortRides(timeStep);
@@ -32,23 +33,28 @@ public class Car {
         return r.ride;
     }
 
-    private PriorityQueue<RideWithDistance> sortRides(Integer timeStep) {
+    private LinkedList<RideWithDistance> sortRides(Integer timeStep) {
         Comparator<RideWithDistance> comparator1 = new RideComparator();
-        PriorityQueue<RideWithDistance> sorted = new PriorityQueue<>(comparator1);
+        LinkedList<RideWithDistance> sorted = new LinkedList<>();
 
         for(RideWithDistance r : sortedRides) {
             if(r.ride.earliestStart - timeStep >= this.distanceFrom(r.ride.fromX, r.ride.fromY))
                 sorted.add(r);
         }
 
-        if(sorted.isEmpty()) {
-            Comparator<RideWithDistance> comparator2 = new RideComparator2();
-            sorted = new PriorityQueue<>(comparator2);
-            for(RideWithDistance r: sortedRides) {
-                if(distanceFrom(r.ride.fromX,r.ride.fromY) + r.ride.length+timeStep < r.ride.latestFinish)
-                    sorted.add(r);
-            }
+        for(RideWithDistance r : sorted) {
+            sortedRides.remove(r);
         }
+        Collections.sort(sorted, comparator1);
+
+        LinkedList<RideWithDistance> sorted2 = new LinkedList<>();
+        Comparator<RideWithDistance> comparator2 = new RideComparator2();
+        for(RideWithDistance r: sortedRides) {
+            if(distanceFrom(r.ride.fromX,r.ride.fromY) + r.ride.length+timeStep < r.ride.latestFinish)
+                sorted.add(r);
+        }
+        Collections.sort(sorted2, comparator2);
+        sorted.addAll(sorted2);
 
         return sorted;
     }
