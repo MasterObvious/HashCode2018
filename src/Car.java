05751ed603140920a86a15efe750ceaf39f04fac
Franz <fgn24@cam.ac.uk>
2018-03-01 19:10:10 +0000

    Add priority queue to Car object with rides closest and possible to make. Add update method that takes timetep and current rides that need to be assigned

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,4 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Car {
     public int x;
@@ -6,13 +6,38 @@ public class Car {
     public int freeFrom;
     public PriorityQueue<Ride> sortedRides;
 
-    public Car(PriorityQueue<Ride> rides){
-        x = 1;
-        y = 1;
+
+    public Car(){
+        x = 0;
+        y = 0;
         freeFrom = 0;
-        sortedRides = sortRides(rides);
+    }
+    public void update(Collection<Ride> rides, Integer timeStep) {
+        sortedRides = sortRides(rides, timeStep);
     }
     public Integer distanceFrom(int x, int y) {
         return Math.abs(this.x-x) + Math.abs(this.y-y);
     }
+    public Ride getShortestDistanceRide() {
+        return sortedRides.peek();
+    }
+
+    private PriorityQueue<Ride> sortRides(Collection<Ride> rides, Integer timeStep) {
+        Comparator<Ride> comparator = new RideComparator();
+        PriorityQueue<Ride> sorted = new PriorityQueue<>(comparator);
+
+        for(Ride r : rides) {
+            if(r.earliestStart - timeStep >= this.distanceFrom(r.fromX, r.fromY))
+                sorted.add(r);
+        }
+
+        return sorted;
+    }
+    private class RideComparator implements Comparator<Ride>{
+        @Override
+        public int compare(Ride o1, Ride o2) {
+            return o1.earliestStart-o2.earliestStart;
+        }
+    }
 }
+
