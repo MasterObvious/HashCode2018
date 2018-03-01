63ae52fa8ad71f54cdc11fa66798649cddb8d26d
Jacob Moxham <jacobmoxham@live.co.uk>
2018-03-01 18:31:34 +0000

    ADDED: rides queue

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,16 @@
+import java.util.PriorityQueue;
+
 public class Car {
     public int x;
     public int y;
     public int freeFrom;
-    public Car(){
+    public PriorityQueue<Ride> sortedRides;
+
+    public Car(PriorityQueue<Ride> rides){
         x = 1;
         y = 1;
         freeFrom = 0;
+        sortedRides = sortRides(rides);
     }
     public Integer distanceFrom(int x, int y) {
         return Math.abs(this.x-x) + Math.abs(this.y-y);
